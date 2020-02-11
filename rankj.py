474c8648268e91637d9aacfa565a7e10c21389b9
mguidoti <marcus.guidoti@gmail.com>
2020-02-11 16:05:34 -0300

    feat: Add rankj.py to the repo

---
+++ rankj.py
@@ -0,0 +1,46 @@
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+results = {}
+
+"""
+if sys.argv[1] == "nsp":
+    in_filename = "results\\WoS_nsp_final.txt"
+elif sys.argv[1] == "newTaxa":
+    in_filename = "results\\WoS_newTaxa_final.txt"
+elif sys.argv[1] == "nomenActs":
+    in_filename = "results\\WoS_nomenActs_final.txt"
+else:
+    print("Please, specify a correct parameter.")
+"""
+
+in_filename = "results\\WoS_nsp_final.txt"
+
+#out_filename = "results\\" + sys.argv[1] + "_out.csv"
+
+out_filename = "results\\" + "nsp" + "_out.csv"
+
+with open(in_filename, encoding='utf-8') as in_file, open(out_filename, 'w') as out_file:
+    for line in in_file:
+        if line[0:2] == "SO":
+            if line[3:][:-1] in results.keys():
+                results[line[3:][:-1]] += 1
+            else:
+                results[line[3:][:-1]] = 1
+
+    results_sorted = sorted(results.items(), key=lambda x: x[1], reverse=True)
+
+    total_papers = 0
+
+    for each in results_sorted:
+        total_papers += each[1]
+
+    out_file.write("Journal;Papers;Proportion;Accumulated\n")
+
+    accumulation = 0
+
+    for each in results_sorted:
+        accumulation += each[1]/total_papers*100
+        processed_line = each[0] + ";" + str(each[1]) + ";" + str(each[1]/total_papers*100) + ";" + str(accumulation) + "\n"
+        out_file.write(processed_line)
