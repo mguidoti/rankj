62454c776c812d5484557b455f8c590f9f3d9726
mguidoti <marcus.guidoti@gmail.com>
2020-05-06 20:49:51 -0300

    feat: Remove print debug statement

--- joinresults.py
+++ joinresults.py
@@ -7,8 +7,6 @@ out_filename = "results\\WoS_" + sys.argv[2] + "_final.txt"
 
 file_list = os.listdir(path)
 
-#print(file_list)
-
 with open(out_filename, "wb") as outfile:
     for f in file_list:
         with open((path + "\\" + f), "rb") as infile:
